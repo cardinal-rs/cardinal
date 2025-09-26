88251b720b34e1a2a41bc029722970737fcd1319
andreespirela <andreespirela@outlook.com>
2025-09-25 23:24:02 -0400

    v0.2.0

--- src/crates/cli/src/cmd/run.rs
+++ src/crates/cli/src/cmd/run.rs
@@ -1,6 +1,6 @@
 use crate::CmdRun;
-use cardinal::Cardinal;
 use cardinal_errors::CardinalError;
+use cardinal_rs::Cardinal;
 
 pub fn run_cmd(cmd: CmdRun) -> Result<(), CardinalError> {
     let cardinal = Cardinal::from_paths(&cmd.config)?;
